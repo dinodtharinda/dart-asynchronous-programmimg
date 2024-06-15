0c7c7699c7d36ae84794e9d99b8e7713c0a50079
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-06-15 20:26:19 +0530

    example 1

--- bin/example1.dart
+++ bin/example1.dart
@@ -1,5 +1,24 @@
-void main(List<String> args) async{
-    print(await getUserName());
+void main(List<String> args) async {
+  print(await getUserName());
+  print(await getAddress());
+  print(await getPhoneNumber());
+  print(await getCity());
+  print(await getCountry());
+  print(await getZipCode());
 }
 
-Future<String> getUserName()async => 'Dinod Tharinda';
+Future<String> getUserName() async => 'Dinod Tharinda';
+Future<String> getAddress() => Future.value('Moratuwa');
+Future<String> getPhoneNumber() =>
+    Future.delayed(const Duration(seconds: 1), () => '555-555-555-55');
+
+Future<String> getCity() async {
+  await Future.delayed(const Duration(seconds: 1));
+  return 'Moratuwa';
+}
+
+Future<String> getCountry() async => "Sri Lanka";
+
+//async keyword doesn't really contribute with anything....
+Future<String> getZipCode() async =>
+    Future.delayed(const Duration(seconds: 1), () => '10400');
